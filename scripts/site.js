3084cdfd00f477d7c9143cfc0d39ca269f9bb529
Max Bucknell <me@maxbucknell.com>
2017-12-16 16:12:18 +0000

    Backport improvements from built version of maxbucknell.com

--- scripts/site.js
+++ scripts/site.js
@@ -6,15 +6,38 @@
 // http://github.com/squarespace/squarespace-core
 
 var core = require('@squarespace/core');
+var katex = require('katex');
 
 window.addEventListener('DOMContentLoaded', function() {
+  renderImages();
+  renderMaths();
+});
 
+function renderImages () {
   var images = document.querySelectorAll('img[data-src]');
 
-  for (var i = 0; i < images.length; i++) {
-    core.ImageLoader.load(images[i], {
-      load: true
-    });
-  }
+  images.forEach(renderImage);
+}
 
-});
+function renderImage (image) {
+  core.ImageLoader.load(image, { load: true });
+}
+
+function renderMaths () {
+  var display = document.querySelectorAll('code.math');
+  var inline = document.querySelectorAll('span.math');
+
+  display.forEach(renderMath.bind(null, true));
+  inline.forEach(renderMath.bind(null, false));
+}
+
+function renderMath (isDisplayMode, el) {
+  var mathText = el.innerText;
+
+  try {
+    katex.render(mathText, el, { displayMode: isDisplayMode });
+  } catch (e) {
+    console.error(e);
+    el.innerHTML = mathText;
+  }
+}
